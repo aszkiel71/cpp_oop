b47532ffb6250f567c2047139adbf2d98e3f6035
Wojtek <returnpll@gmail.com>
2025-01-25 16:44:18 +0100

    oop_day1

---
+++ first-file.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+    public:
+    int age;
+    string name;
+    string uni;
+    int learn();
+
+};
+
+
+class Animal{
+    public:
+    //attributes
+    string family;
+    string name;
+    int age;
+
+    //methods
+    void add_animal()
+    {
+        cout<<"Adding an animal"<<endl;
+        cout<<"Enter family:";
+        cin >> family;
+        cout<<"Enter name:";
+        cin >> name;
+        cout<<"Enter age:";
+        cin >> age;
+        cout<<endl;
+    }
+
+    void display()
+    {
+        cout<<"Family: "<<family<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+
+};
+
+
+int main() {
+    Student Wojtek;
+    Wojtek.age = 18;
+    Wojtek.name = "Wojtek";
+    Wojtek.uni = "University of Wroclaw";
+    cout << Wojtek.age << endl << Wojtek.name << endl << Wojtek.uni << endl;
+
+    Animal cow;
+    cow.add_animal();
+    cow.display();
+
+
+    return 0;
+}
